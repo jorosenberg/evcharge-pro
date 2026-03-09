f09e6e9175823a515f15cae2f9e5ad73674ea79d
jorosenberg <jorosenberg@users.noreply.github.com>
2026-03-09 19:38:11 +0000

    deploy: 2bdd48544fd8a75ccca72b0375071c8b3b8ee0c6

---
+++ config.js
@@ -0,0 +1,8 @@
+window.EVCHARGE_CONFIG = { ocmKey: "8e20f760-d3b0-467f-9860-96cd04779ce2", firebase: {
+  "apiKey": "AIzaSyBBijkETAgvlaWMk2CqK0qeIosU2Z9Nb7s",
+  "authDomain": "evcharge-pro.firebaseapp.com",
+  "projectId": "evcharge-pro",
+  "storageBucket": "evcharge-pro.firebasestorage.app",
+  "messagingSenderId": "1041654680644",
+  "appId": "1:1041654680644:web:8e58380a2b0326476c0011"
+} };
